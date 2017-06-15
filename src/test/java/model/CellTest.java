4aeeacdd90e5880d7d09e52a76cbe220702c303b
Anna Matras <ania.matras@gmail.com>
2017-06-15 14:10:06 +0000

    Refactor test for Cell class

--- src/test/java/model/CellTest.java
+++ src/test/java/model/CellTest.java
@@ -1,58 +1,37 @@
-//package model;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import type.Seed;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//
-//class CellTest {
-//    @BeforeEach
-//    void setUp() {
-//
-//    }
-//
-//    @Test
-//    void testIfConstructorSetValidRow() {
-//        Cell cell = new Cell(1, 2);
-//        int expectedRow = cell.getRow();
-//        assertEquals(expectedRow, 1);
-//    }
-//
-//    @Test
-//    void testIfConstructorSetValidCol() {
-//        Cell cell = new Cell(1, 2);
-//        int expectedCol = cell.getCol();
-//        assertEquals(expectedCol, 2);
-//    }
-//
-//    @Test
-//    void testIfCellSetValidContent() {
-//        Cell cell = new Cell(1, 3);
-//        cell.setContent(Seed.CROSS);
-//        assertEquals(Seed.CROSS, cell.getContent());
-//    }
-//
-//    @Test
-//    void testIfContentIsNullThrowsNullPointerException() {
-//        Cell cell = new Cell(1, 3);
-//        assertThrows(NullPointerException.class, () -> {
-//            cell.setContent(null);
-//        });
-//    }
-//
-//    @Test
-//    void testIfSetColOutOfValidRangeThrowsIllegalArgumentException() throws Exception {
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            Cell cell = new Cell(1, 5);
-//        });
-//    }
-//
-//    @Test
-//    void testIfSetRowOutOfValidRangeThrowsIllegalArgumentException() throws Exception {
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            Cell cell = new Cell(10, 2);
-//        });
-//    }
-//}
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import type.Seed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CellTest {
+
+    Cell cell;
+
+    @BeforeEach
+    void setUp() {
+        cell = new Cell();
+    }
+
+    @Test
+    void testIfContentIsNullThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> {
+            cell.setContent(null);
+        });
+    }
+
+    @Test
+    void testIfContentSetSeedToEmpty() {
+        cell.setContent(Seed.EMPTY);
+        assertEquals(Seed.EMPTY, cell.getContent());
+    }
+
+    @Test
+    void testIfContentSetSeedToCross() {
+        cell.setContent(Seed.CROSS);
+        assertEquals(Seed.CROSS, cell.getContent());
+    }
+}
