516cf8c04eb28bebf1760aeed329819947d2cce8
Anna Matras <ania.matras@gmail.com>
2017-06-15 14:59:27 +0000

    Add test for get/set method

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1,8 +1,35 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import type.GameState;
+import type.Seed;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class GameTest {
 
+    Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    void testIfCurrentPlayerSetProperly() {
+        game.setCurrentPlayer(Seed.EMPTY);
+        assertEquals(Seed.EMPTY, game.getCurrentPlayer());
+    }
+
+    @Test
+    void testIfCurrentStateSetProperly() {
+        game.setCurrentState(GameState.DRAW);
+        assertEquals(GameState.DRAW, game.getCurrentState());
+    }
+
+    @Test
+    void testIfMethodPickPlayerFindValidPlayer() {
+    }
 }
