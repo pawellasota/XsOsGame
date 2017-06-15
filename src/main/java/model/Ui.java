ff471ff9a38c56b1a6d404e42a65924c78b98054
Anna Matras <ania.matras@gmail.com>
2017-06-15 14:59:11 +0000

    Refactor

--- src/main/java/model/Ui.java
+++ src/main/java/model/Ui.java
@@ -12,12 +12,13 @@ public class Ui {
     public static void printBoard(Cell[] board) {
 
         String boardString = new String(
-                    "-------\n|" + printCell(board[0].getContent()) + "|"+printCell(board[1].getContent())+"|"+printCell(board[2].getContent())+"|\n" +
-                           "-------\n|" + printCell(board[3].getContent()) + "|"+printCell(board[4].getContent())+"|"+printCell(board[5].getContent())+"|\n" +
-                           "-------\n|" + printCell(board[6].getContent()) + "|"+printCell(board[7].getContent())+"|"+printCell(board[8].getContent())+"|\n" +
-                           "-------\n");
+                "-------\n|" + printCell(board[0].getContent()) + "|" + printCell(board[1].getContent()) + "|" + printCell(board[2].getContent()) + "|\n" +
+                        "-------\n|" + printCell(board[3].getContent()) + "|" + printCell(board[4].getContent()) + "|" + printCell(board[5].getContent()) + "|\n" +
+                        "-------\n|" + printCell(board[6].getContent()) + "|" + printCell(board[7].getContent()) + "|" + printCell(board[8].getContent()) + "|\n" +
+                        "-------\n");
 
         System.out.println(boardString);
+    }
 
 
     public static void print(String value) {
